296a8f6348709d6515f00993e5314863850747fa
Benjamin Jansson <benjamin.jansson@learnet.se>
2022-03-02 16:55:58 +0100

    Add project files.

---
+++ Pärlhalsband/Pearlbag.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PearlNecklace;
+
+
+namespace PearlNecklace
+{
+    class Pearlbag : IPearlbag
+    {
+        List<Pearl> _pearls = new List<Pearl>();
+
+        public void Sort()
+        {
+            _pearls.Sort();
+        }
+
+        public int Find(Pearl otherPearl)
+        {
+            int matches = 0;
+            foreach (var pearl in _pearls)
+            {
+                if (pearl.Equals(otherPearl))
+                {
+                    matches++;
+                }
+            }
+
+            return matches;
+        }
+    }
+}

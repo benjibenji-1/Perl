f14b6704cb5a00759b17f7402e54b2c05782dd42
benjibenji-1 <bennejansson531@gmail.com>
2022-03-02 16:55:58 +0100

    Add project files.

---
+++ Pärlhalsband/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+using PearlNecklace;
+
+var pearlBag = new Pearlbag();
+for (int i = 0; i < 35; i++)
+{
+    pearlBag.
+}
+
+Pearl pearlTest = new Pearl();
+
+
+int matches = 0;
+foreach (var pearl in pearlBag)
+{
+    if (pearl.Equals(pearlTest))
+    {
+        matches++;
+    }
+}

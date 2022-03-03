342df9988b87002743f7102632ec5249d3626f3e
Benjamin Jansson <benjamin.jansson@learnet.se>
2022-03-03 12:59:29 +0100

    Completed PearlBag

--- Pärlhalsband/Program.cs
+++ Pärlhalsband/Program.cs
@@ -2,19 +2,5 @@
 using PearlNecklace;
 
 var pearlBag = new Pearlbag();
-for (int i = 0; i < 35; i++)
-{
-    pearlBag.
-}
-
 Pearl pearlTest = new Pearl();
-
-
-int matches = 0;
-foreach (var pearl in pearlBag)
-{
-    if (pearl.Equals(pearlTest))
-    {
-        matches++;
-    }
-}
+Console.WriteLine($"Number of identical pearls: {pearlBag.Find(pearlTest)}");

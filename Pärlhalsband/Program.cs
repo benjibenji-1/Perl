ffce3f272b4f28b02187fd70a3e408be7e271659
JMetropolis <jcityes@gmail.com>
2022-03-10 14:32:52 +0100

    Query to db 1k necklaces

--- Pärlhalsband/Program.cs
+++ Pärlhalsband/Program.cs
@@ -14,3 +14,21 @@ Console.WriteLine("Create a couple of Random pearls");
 Console.WriteLine(Pearl.Factory.CreateRandomPearl());
 Console.WriteLine(Pearl.Factory.CreateRandomPearl());
 
+//Adding 1000 necklaces
+var necklaceListTest = new List<Necklace>();
+var amountOfNecklaces = 1000;
+for (int i = 0; i < amountOfNecklaces; i++)
+{
+	necklaceListTest.Add(new Necklace());
+}
+
+necklaceListTest.ForEach(necklace => database.Necklaces.Add(necklace));
+database.SaveChanges();
+
+
+//foreach (var necklace in necklaceListTest)
+//{
+//	Console.WriteLine(necklace);
+//}
+
+Console.WriteLine(necklaceListTest.Max(t => t.price));

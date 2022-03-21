7fd3c00251436cff612f346ff7013aa20b6cd6ad
Faultline94 <faultline313@gmail.com>
2022-03-21 10:56:10 +0100

    First changes CRUD

---
+++ Pärlhalsband/INecklaceRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PearlNecklace
+{
+    public interface INecklaceRepository
+        {
+            Task<Necklace> CreateAsync(Necklace neck);
+            Task<IEnumerable<Necklace>> ReadAllAsync();
+            Task<Necklace> ReadAsync(Guid necklaceId);
+            Task<Necklace> UpdateAsync(Necklace neck);
+            Task<Necklace> DeleteAsync(Guid necklaceId);
+        }
+    }
+}
+}
---
+++ Pärlhalsband/NecklaceRepository.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PearlNecklace
+{
+    public class NecklaceRepository : INecklaceRepository
+    {
+        NecklaceDb _db = null;
+
+        public async Task<Necklace> CreateAsync(Necklace neck)
+        {
+            var added = await _db.Necklaces.AddAsync(neck);
+
+            int affected = await _db.SaveChangesAsync();
+            if (affected == 1)
+                return neck;
+            else
+                return null;
+        }
+
+        public async Task<Necklace> DeleteAsync(Guid necklaceId)
+        {
+            var neckDel = await _db.Necklaces.FindAsync(necklaceId);
+            _db.Necklaces.Remove(neckDel);
+
+            int affected = await _db.SaveChangesAsync();
+            if (affected == 1)
+                return neckDel;
+            else
+                return null;
+        }
+
+        public async Task<IEnumerable<Necklace>> ReadAllAsync()
+        {
+            return await Task.Run(() => _db.Necklaces);
+        }
+
+        public async Task<Necklace> ReadAsync(Guid necklaceId)
+        {
+            return await _db.Necklaces.FindAsync(necklaceId);
+        }
+
+        public async Task<Necklace> UpdateAsync(Necklace neck)
+        {
+            _db.Necklaces.Update(neck);
+            int affected = await _db.SaveChangesAsync();
+            if (affected == 1)
+                return neck;
+            else
+                return null;
+        }
+
+        public NecklaceRepository(NecklaceDb db)
+        {
+            _db = db;
+        }
+    }
+}

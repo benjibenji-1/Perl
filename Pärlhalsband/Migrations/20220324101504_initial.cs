343b312ed75f1140585f26868887cbc7a0f2b523
benjibenji-1 <bennejansson531@gmail.com>
2022-03-24 11:19:05 +0100

    Fixed connection string

---
+++ Pärlhalsband/Migrations/20220324101504_initial.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PearlNecklace.Migrations
+{
+    public partial class initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Necklaces",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    price = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Necklaces", x => x.ID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Pearls",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    necklaceID = table.Column<int>(type: "int", nullable: false),
+                    Size = table.Column<int>(type: "int", nullable: false),
+                    Color = table.Column<int>(type: "int", nullable: false),
+                    Shape = table.Column<int>(type: "int", nullable: false),
+                    Type = table.Column<int>(type: "int", nullable: false),
+                    Price = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Pearls", x => x.ID);
+                    table.ForeignKey(
+                        name: "FK_Pearls_Necklaces_necklaceID",
+                        column: x => x.necklaceID,
+                        principalTable: "Necklaces",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Pearls_necklaceID",
+                table: "Pearls",
+                column: "necklaceID");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Pearls");
+
+            migrationBuilder.DropTable(
+                name: "Necklaces");
+        }
+    }
+}

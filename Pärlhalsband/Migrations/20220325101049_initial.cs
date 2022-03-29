f7c191a3899ffe585b46238c7dac3b4220b77ea0
Benjamin Jansson <benjamin.jansson@learnet.se>
2022-03-29 14:43:28 +0200

    Added ReadAllAsyncWithPearls to INecklaceRepository

--- Pärlhalsband/Migrations/20220325101049_initial.cs
+++ Pärlhalsband/Migrations/20220325101049_initial.cs
@@ -4,7 +4,7 @@
 
 namespace PearlNecklace.Migrations
 {
-    public partial class Init : Migration
+    public partial class initial : Migration
     {
         protected override void Up(MigrationBuilder migrationBuilder)
         {
